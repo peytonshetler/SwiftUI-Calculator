5dd335632619a50c26d1fc5a121a7752a4b6942b
Peyton Shetler <peyton.shetler@gmail.com>
2022-04-04 20:19:02 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Peyton Shetler on 4/4/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
